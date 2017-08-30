d6173501e8e5b4e6e8b6a1f2bc8d14d0aa92d68b
pallavi priya <pallavi.priya@bluepandatec.com>
2017-08-30 04:37:06 +0000

    start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
-# cd /tmp
+# rm -rf nodedocker; true
+# mkdir nodedocker_tmp
+# cd /nodedocker_tmp
 
 # # try to remove the repo if it already exists
-# rm -rf nodedocker; true
+rm -rf nodedocker; true
 
-# git clone https://github.com/pallaviataws/nodedocker.git
+git clone https://github.com/pallaviataws/nodedocker.git
 
-cd nodedocker
+#cd nodedocker
 
 # npm install
 
-npm start
+#npm start
