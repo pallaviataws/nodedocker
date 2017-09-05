40665ea89770ed4d6c7ca1f135d17b2b655f139c
pallavi priya <pallavi.priya@bluepandatec.com>
2017-09-05 04:05:02 +0000

    nginx docker image

--- build.sh
+++ build.sh
@@ -6,22 +6,22 @@ sudo docker version
 
 # build the image based on the Dockerfile and name it `nvm`
 # cache should be enabled. the git should pull the code everytime, if cache is enabled
-sudo docker build  -t nvm .
+sudo docker build  -t dhiraj/nvm .
 
 # confirm image is present
 sudo docker images
 
 # enter container terminal
-sudo docker run -d -p 3000:3000 --name nodeApp nvm 
+sudo docker run -d -p 3000:3000 --name nodeApp dhiraj/nvm 
 #sudo docker run -d -p 3000:3000  nvm
 
 # run docker with nginx
 cd nginx
 
-sudo docker build -t nginx .
+sudo docker build -t dhiraj/nginx .
 # sudo docker run -p 8000:80 -d nginx
 
-sudo docker run -d -p 8000:80 --link nodeApp:app --name nginxProxy nginx
+sudo docker run -p 8000:80 --link nodeApp:app  dhiraj/nginx
 
 #sudo docker run -d -p 8000:80  nginx
 
